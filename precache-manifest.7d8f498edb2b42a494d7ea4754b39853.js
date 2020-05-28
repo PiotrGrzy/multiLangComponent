021ec22b6b369a9121018ec900c5805632b0e03a
PiotrGrzy <piotr.grzymowicz1@gmail.com>
2020-05-28 17:56:35 +0200

    Updates

---
+++ precache-manifest.7d8f498edb2b42a494d7ea4754b39853.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ac9c8d9d6fa0c1f031b5dc9cecab66",
+    "url": "/multiLangComponent/index.html"
+  },
+  {
+    "revision": "6df62362ffb9262d058c",
+    "url": "/multiLangComponent/static/js/2.54c9980b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/multiLangComponent/static/js/2.54c9980b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fceb896578f81681e70b",
+    "url": "/multiLangComponent/static/js/main.5f9b6c30.chunk.js"
+  },
+  {
+    "revision": "df7d1670c2a77f304600",
+    "url": "/multiLangComponent/static/js/runtime-main.a685bff4.js"
+  }
+]);
